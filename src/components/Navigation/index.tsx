e3e4c91e0f1419b688c35c3b2d2bd4034606e9ff
DanijelAdrinek <dadrinek00@gmail.com>
2024-05-16 02:47:48 -0700

    Nav component update

--- src/components/Navigation/index.tsx
+++ src/components/Navigation/index.tsx
@@ -6,26 +6,19 @@ import Link from 'next/link';
 import Image from 'next/image';
 import Logo from '@public/images/face_black_noBG.svg';
 
-
+// This is the Navigation component that I was making while I still didnt know what the project would look like and what it would do
+//  this component did not make it to the final project
 function Navigation() {
-
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-
-    const toggleMenu = () => setIsMenuOpen(!isMenuOpen);
-
     return (
         <nav className={styles.nav}>
             <div className={styles['nav-content-container']}>
                 <Image src={Logo} alt='Logo of my face' width={50} height={50} className={styles.image}/>
-                <div className={`${styles['nav-links']} ${isMenuOpen ? styles['open'] : ''}`}>
+                <div className={styles['nav-links']}>
                     <Link href="/">Home</Link>
                     <Link href="/about">About Me</Link>
                     <Link href="/projects">Projects</Link>
                     <Link href="/contact">Contact Me</Link>
                 </div>
-                <button onClick={toggleMenu} className={`${styles['menu-btn-container']} not_tablet not_desktop`}>
-                    <div className={`${styles['menu-btn']}`}></div>
-                </button>
             </div>
         </nav>
     );
