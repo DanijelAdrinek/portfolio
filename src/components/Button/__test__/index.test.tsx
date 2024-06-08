2c4e393d4b98ecc8760847a54d5fc20b268f812c
DanijelAdrinek <dadrinek00@gmail.com>
2024-06-08 03:21:57 -0700

    finished testing button

--- src/components/Button/__test__/index.test.tsx
+++ src/components/Button/__test__/index.test.tsx
@@ -3,6 +3,7 @@ import { render, screen, fireEvent } from '@testing-library/react';
 import Button from '..';
 import { BUTTON_SIZES } from '@/constants';
 import Styles from '../styles.module.css';
+import exp from 'constants';
 
 it("Should render Button to DOM", () => {
     render(<Button>Hello World</Button>);
@@ -36,32 +37,45 @@ it("Should make Button disabled", () => {
   expect(buttonElement).toBeDisabled();
 });
 
-it("Should make button have a large size", () => {
-  render(<Button size={BUTTON_SIZES.LARGE}>Hello World</Button>);
-
-  const buttonElement = screen.getByRole('button');
-
-  expect(buttonElement).toHaveClass(Styles[BUTTON_SIZES.LARGE]);
+describe("Button sizes", () => {
+  it("Should make button have a large size", () => {
+    render(<Button size={BUTTON_SIZES.LARGE}>Hello World</Button>);
+  
+    const buttonElement = screen.getByRole('button');
+  
+    expect(buttonElement).toHaveClass(Styles[BUTTON_SIZES.LARGE]);
+  });
+  
+  it("Should make button have a small size", () => {
+    render(<Button size={BUTTON_SIZES.SMALL}>Hello World</Button>);
+  
+    const buttonElement = screen.getByRole('button');
+  
+    expect(buttonElement).toHaveClass(Styles[BUTTON_SIZES.SMALL]);
+  });
+  
+  it("Should make button with size of medium have same classes as button without size", () => {
+    render(
+      <>
+        <Button size={BUTTON_SIZES.MEDIUM}>Medium</Button>
+        <Button>Hello World</Button>
+      </>
+    );
+    const buttonWithMediumSize = screen.getByRole('button', { name: 'Medium' });
+    const buttonWithoutSize = screen.getByRole('button', { name: 'Hello World' });
+  
+    expect(buttonWithMediumSize.className).toEqual(buttonWithoutSize.className);
+  });
 });
 
-it("Should make button have a small size", () => {
-  render(<Button size={BUTTON_SIZES.SMALL}>Hello World</Button>);
+it("Should call mocked function if button is clicked", () => {
+  const mockedFn = vitest.fn();
+  render(<Button clickFunction={() => { mockedFn() }}>Hello World</Button>);
 
   const buttonElement = screen.getByRole('button');
-
-  expect(buttonElement).toHaveClass(Styles[BUTTON_SIZES.SMALL]);
-});
-
-const mockedFn = vitest.fn();
-
-it("Should change text content of button if button is clicked", () => {
-  render(<Button id='test-btn' clickFunction={() => { mockedFn() }}>Hello World</Button>);
-
-  const buttonElement = screen.getByRole('button');
-
   fireEvent.click(buttonElement);
 
-  expect(buttonElement);
+  expect(mockedFn).toHaveBeenCalledOnce();
 });
 
 it("Should give button a type of submit", () => {
@@ -69,5 +83,57 @@ it("Should give button a type of submit", () => {
 
   const buttonElement = screen.getByRole('button', { target: { type: 'submit'} });
 
-  expect(buttonElement);
-})
+  expect(buttonElement).toBeDefined();
+});
+
+it("Should have the background color we assign to it", () => {
+  const bgColor = 'rgb(0, 0, 0)';
+  render(<Button backgroundColor={bgColor}>Hello World</Button>);
+
+  const buttonElement = screen.getByRole('button');
+
+  const ButtonStyles = window.getComputedStyle(buttonElement);
+
+  expect(ButtonStyles.backgroundColor).toBe(bgColor);
+});
+
+it("Should have the color we assign to it", () => {
+  const color = 'rgb(255, 255, 255)';
+  render(<Button color={color}>Hello World</Button>);
+
+  const buttonElement = screen.getByRole('button');
+
+  const ButtonStyles = window.getComputedStyle(buttonElement);
+
+  expect(ButtonStyles.color).toBe(color);
+});
+
+it("Should have the border we assign to it", () => {
+  const borderStyles = '1px solid rgb(255, 255, 255)';
+  render(<Button border={borderStyles}>Hello World</Button>);
+
+  const buttonElement = screen.getByRole('button');
+
+  const ButtonStyles = window.getComputedStyle(buttonElement);
+
+  expect(ButtonStyles.border).toBe(borderStyles);
+});
+
+it("Should add classes we add to className", () => {
+  const addedClass = 'test-class';
+  
+  render(<Button className={addedClass}>Hello World</Button>);
+
+  const buttonElement = screen.getByRole('button');
+
+  expect(buttonElement).toHaveClass(addedClass);
+});
+
+it("Should add id we pass to it to the button", () => {
+  const addedId = 'test-id';
+  render(<Button id={addedId}>Hello World</Button>);
+
+  const buttonElement = screen.getByRole('button');
+
+  expect(buttonElement).toHaveAttribute('id', addedId);
+});
